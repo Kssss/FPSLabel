5baf3631084ed1a67bf8e361c242da59d729e4ad
Vieene <5652903@qq.com>
2016-11-28 18:44:58 +0800

    初始化 项目

---
+++ FPSLabel.h
@@ -0,0 +1,14 @@
+//
+//  FPSLabel.h
+//
+//  Created by Vieene on 15/11/16.
+//  Copyright © 2015年 Vieene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPSLabel : UILabel
+
+- (void)invalidate;
+
+@end
